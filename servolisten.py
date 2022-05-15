815940a67150722e1aaadaa1ebc301f5082f1338
RoboticNerd <70571400+RoboticNerd@users.noreply.github.com>
2022-05-15 16:58:16 -0500

    Create servolisten.py

---
+++ servolisten.py
@@ -0,0 +1,39 @@
+import rclpy
+from rclpy.node import Node
+from gpiozero import Servo
+from time import sleep
+
+from std_msgs.msg import Int32
+
+
+class ServoListen(Node):
+
+    def __init__(self):
+        super().__init__('servo_listen')
+        self.subscription = self.create_subscription(
+            Int32,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    servo_listen = ServoListen()
+
+    rclpy.spin(servo_listen)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    servo_listen.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
